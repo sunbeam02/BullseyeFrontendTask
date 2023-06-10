677001fec7c99801bab1a73eed11b0097076bb17
Olaleye Samuel <Olaleye.S@bullseye.local>
2023-06-10 13:42:13 +0100

    third commit

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const search = document.getElementById('search')
 search.addEventListener('click', (e)=>{
     e.preventDefault() 
     
-    var Jobfilt = Job.filter((job))=>
+    var Jobfilt = Job.filter((job)=>true)
 })
